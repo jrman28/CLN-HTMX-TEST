5ff4b6cf66a52d8e49f5221b6dcad0eeac1ac54b
jrman28 <jrman28@gmail.com>
2024-02-09 13:19:30 -0500

    Create .py

---
+++ .py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import json
+import re
+from pyln.client import Plugin, RpcError
+
+plugin = Plugin()
+
+@plugin.init()  # this runs when the plugin starts.
+def init(options, configuration, plugin, **kwargs):
+
+    plugin.log("lnplay.live - rpc plugin initialized")
+
+
+@plugin.method("htmx-getinfo")
+def htmx_getinfo(plugin):
+    '''Returns the getinfo output as HTMX.'''
+    try:
+        return "<p>Get Info</p>"
+
+    except RpcError as e:
+        plugin.log(e)
+        return e
+
+
+
+plugin.run()  # Run our plugin
