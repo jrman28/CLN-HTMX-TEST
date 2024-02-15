b1fec874c5f7b72942d6a2aa1b5d6d836c4f1614
Derek Smith <derek@farscapian.com>
2024-02-15 14:17:55 -0500

    Update cln-htmx-example.

--- cln-htmx-getinfo.py
+++ cln-htmx-getinfo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from flask import Flask, Response, render_template
 from pyln.client import Plugin, RpcError
 
 plugin = Plugin()
@@ -13,12 +14,10 @@ def init(options, configuration, plugin, **kwargs):
 @plugin.method("htmx-getinfo")
 def htmx_getinfo(plugin):
     '''Returns the getinfo output as HTMX.'''
-    try:
-        return "<p>Get Info</p>"
+    
+    html_content = "<table><tr><th>Node ID</th><th>Alias</th><th>Color</th></tr><tr><td>node_id_xxx</td><td>Alice</td><td>Blue</td></tr></table>"
 
-    except RpcError as e:
-        plugin.log(e)
-        return e
-
-
+    # Return the HTML content with the correct Content-Type header
+    return Response(html_content, mimetype='text/html')
+    
 plugin.run()  # Run our plugin
