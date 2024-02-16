eb7ba08d9420528cb8fc0e80fa6e38a96f53d66d
Derek Smith <derek@farscapian.com>
2024-02-16 15:12:46 -0500

    Fix Table.

--- cln-htmx-getinfo.py
+++ cln-htmx-getinfo.py
@@ -20,7 +20,7 @@ def htmx_getinfo(plugin):
     node_alias = get_info_response["alias"]
     block_height = get_info_response["blockheight"]
 
-    html_content = f"<table><tr><th>Field</th><th>Value</th></tr><tr><td>Node ID:</td><td>{node_id}</td></tr><td>Alias</td><td>{node_alias}</td></tr></td>Color</td><td>{node_color}</td></tr><tr><td>Block Height</td><td>{block_height}</td></tr></table>"
+    html_content = f"<table><tr><th>Field</th><th>Value</th></tr><tr><td>Node ID:</td><td>{node_id}</td></tr><td>Alias</td><td>{node_alias}</td></tr><tr><td>Color</td><td>{node_color}</td></tr><tr><td>Block Height</td><td>{block_height}</td></tr></table>"
 
     return html_content
 
